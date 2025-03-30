c33a2e73319dbd10f671c2d59f00860549e91e3e
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-03-30 19:49:42 -0400

    Update Layout.jsx

--- react-01/src/Layout.jsx
+++ react-01/src/Layout.jsx
@@ -5,11 +5,15 @@ import Navigation from './components/Navigation';
 
 function Layout() {
   return (
-    <>
+    <div className="app-layout">
+      {/* Site header and nav are persistent across pages */}
       <Header />
       <Navigation />
-      <Outlet />
-    </>
+      {/* Outlet renders the matched page component */}
+      <main className="content">
+        <Outlet />
+      </main>
+    </div>
   );
 }
 
