a6979fdaa0ad0a848c95e9029b3dd5a0343140c0
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-03-30 19:56:46 -0400

    Update Home.jsx

--- react-01/src/pages/Home.jsx
+++ react-01/src/pages/Home.jsx
@@ -4,8 +4,13 @@ import './css/Home.css';
 function Home() {
   return (
     <div className="home">
-      <h2>Welcome to My Travel Blog!</h2>
-      <p>Join me as I share my adventures and travel tips from around the world.</p>
+      <h2>Welcome to Our Travel Blog</h2>
+      <p>
+        Join us as we share adventures from around the world. Our travel blog brings you inspiring stories, travel tips, and beautiful photos from countless journeys.
+      </p>
+      <p>
+        Whether you're planning your next trip or just looking for a virtual escape, we hope our experiences ignite your wanderlust. Explore the world with us, one post at a time!
+      </p>
     </div>
   );
 }
