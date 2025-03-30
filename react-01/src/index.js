8ae4345ed8d7a19c5b89f01271c2511accfa1988
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-03-30 19:26:09 -0400

    Update index.js

--- react-01/src/index.js
+++ react-01/src/index.js
@@ -1,31 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import {BrowserRouter, Routes, Route} from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import Layout from './Layout';
 import Home from './pages/Home';
-import Blog from './pages/Blog';
 import About from './pages/About';
+import Blog from './pages/Blog';
 import Nopage from './pages/Nopage';
-
-function App() {
-  return(
-    <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />} />
-          <Route path="about" element={<About />} />
-          <Route path="blog" element={<Blog />} />
-          <Route path="*" element={<Nopage/>} />
-        </Route>
-      </Routes>
-    </BrowserRouter>
-  );
-};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <BrowserRouter>
+    <Routes>
+      {/* Layout wrapper route */}
+      <Route path="/" element={<Layout />}>
+        {/* index = default page for base path */}
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="blog" element={<Blog />} />
+        {/* catch-all for undefined routes */}
+        <Route path="*" element={<Nopage />} />
+      </Route>
+    </Routes>
+  </BrowserRouter>
 );
