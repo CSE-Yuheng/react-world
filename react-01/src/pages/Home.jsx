6f456b70a4fc7a4facd180aedf28be740dbff67a
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-03-30 19:18:20 -0400

    Create Home.jsx

---
+++ react-01/src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './css/Home.css';
+
+function Home() {
+  return (
+    <div className="home">
+      <h2>Welcome to My Travel Blog!</h2>
+      <p>Join me as I share my adventures and travel tips from around the world.</p>
+    </div>
+  );
+}
+
+export default Home;
