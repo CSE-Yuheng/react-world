62e9b5a5c6d30386d93b502bd2cf42fc5d22fc52
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-03-30 19:18:53 -0400

    Update About.jsx

--- react-01/src/pages/About.jsx
+++ react-01/src/pages/About.jsx
@@ -1,11 +1,14 @@
+import React from 'react';
 import './css/About.css';
 
 function About() {
-    return (
-        <>
-            <h1>About</h1>
-        </>
-    );
-};
+  return (
+    <div className="about">
+      <h2>About This Blog</h2>
+      <p>This travel blog is a personal journal of my journeys across the globe.</p>
+      <p>I share destination guides, travel hacks, and stories to inspire others to explore the world.</p>
+    </div>
+  );
+}
 
 export default About;
