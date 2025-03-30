81b695470b4ca989be0e104e7e1019e6e18a453d
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-03-30 19:17:05 -0400

    Update Navigation.jsx

--- react-01/src/components/Navigation.jsx
+++ react-01/src/components/Navigation.jsx
@@ -1,17 +1,17 @@
-import {Outlet, Link} from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 import './css/Navigation.css';
 
-
 function Navigation() {
-    return (
-        <nav id="main-nav">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/blog">Blog</Link></li>
-            </ul>
-        </nav>
-    );
-};
+  return (
+    <nav className="nav">
+      <ul className="nav-list">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+      </ul>
+    </nav>
+  );
+}
 
 export default Navigation;
