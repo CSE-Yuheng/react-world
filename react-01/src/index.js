9c0e1f45c9cbb9ce5c412b4476bcd66970aa0be3
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-04-06 16:48:00 -0400

    Update index.js

--- react-01/src/index.js
+++ react-01/src/index.js
@@ -1,3 +1,4 @@
+// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
@@ -5,23 +6,21 @@ import Layout from './Layout';
 import Home from './pages/Home';
 import About from './pages/About';
 import Blog from './pages/Blog';
+import Contact from './pages/Contact';
 import NoPage from './pages/NoPage';
-import './index.css';  /* Global styles */
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Define a layout route that wraps the main structure */}
+        {/* Layout wraps header and nav */}
         <Route path="/" element={<Layout />}>
-          {/* Index route for the home page */}
           <Route index element={<Home />} />
-          {/* About page route */}
           <Route path="about" element={<About />} />
-          {/* Blog page route */}
           <Route path="blog" element={<Blog />} />
-          {/* Catch-all route for 404 */}
+          <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
       </Routes>
