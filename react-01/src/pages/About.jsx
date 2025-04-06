10e73b75fc0b99bd0ff95fd4b16af25025dfe47d
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-04-06 17:13:55 -0400

    Update About.jsx

--- react-01/src/pages/About.jsx
+++ react-01/src/pages/About.jsx
@@ -1,3 +1,4 @@
+// src/pages/About.jsx
 import React from 'react';
 import './css/About.css';
 
@@ -6,10 +7,7 @@ function About() {
     <div className="about">
       <h2>About This Blog</h2>
       <p>
-        Traveling opens up a world of experiences. This blog was created to document journeys across continents, cultures, and cuisines. We believe that every destination has a story to tell.
-      </p>
-      <p>
-        Our mission is to inspire and inform fellow travelers. Here you'll find travel itineraries, tips for budget traveling, cultural insights, and much more. We hope our stories encourage you to pack your bags and explore the world around you.
+        Our travel blog is dedicated to sharing our journey, tips, and experiences from around the world. We hope our adventures inspire your next trip!
       </p>
     </div>
   );
