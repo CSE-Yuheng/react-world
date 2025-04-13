037ebe63d60264a4123b16dcd438a040fdf27f60
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-04-13 17:45:24 -0400

    Update index.js

--- react-01/src/index.js
+++ react-01/src/index.js
@@ -1,11 +1,10 @@
-// src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from './Layout';
 import Home from './pages/Home';
 import About from './pages/About';
-import Blog from './pages/Blog';
+import Tools from './pages/Tools';
 import Contact from './pages/Contact';
 import NoPage from './pages/NoPage';
 import './index.css';
@@ -15,11 +14,10 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
-        {/* Layout wraps header and nav */}
         <Route path="/" element={<Layout />}>
           <Route index element={<Home />} />
           <Route path="about" element={<About />} />
-          <Route path="blog" element={<Blog />} />
+          <Route path="tools" element={<Tools />} />
           <Route path="contact" element={<Contact />} />
           <Route path="*" element={<NoPage />} />
         </Route>
