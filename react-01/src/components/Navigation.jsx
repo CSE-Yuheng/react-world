14daa82bfec4029039f29719b694e4d3e3890823
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-04-13 17:47:17 -0400

    Update Navigation.jsx

--- react-01/src/components/Navigation.jsx
+++ react-01/src/components/Navigation.jsx
@@ -1,4 +1,3 @@
-// src/components/Navigation.jsx
 import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
 import './css/Navigation.css';
@@ -14,29 +13,14 @@ function Navigation() {
         &#9776;
       </button>
       <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
-        <li>
-          <NavLink to="/" end onClick={() => setMenuOpen(false)}>
-            Home
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/about" onClick={() => setMenuOpen(false)}>
-            About
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/blog" onClick={() => setMenuOpen(false)}>
-            Blog
-          </NavLink>
-        </li>
-        <li>
-          <NavLink to="/contact" onClick={() => setMenuOpen(false)}>
-            Contact
-          </NavLink>
-        </li>
+        <li><NavLink to="/" end onClick={() => setMenuOpen(false)}>Home</NavLink></li>
+        <li><NavLink to="/about" onClick={() => setMenuOpen(false)}>About</NavLink></li>
+        <li><NavLink to="/tools" onClick={() => setMenuOpen(false)}>Tools</NavLink></li>
+        <li><NavLink to="/contact" onClick={() => setMenuOpen(false)}>Contact</NavLink></li>
       </ul>
     </nav>
   );
 }
 
 export default Navigation;
+
