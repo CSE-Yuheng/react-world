db60a7e47ea88e1edc022857ee8e5adeac53f773
CSE-Yuheng <160076022+CSE-Yuheng@users.noreply.github.com>
2025-04-13 18:10:24 -0400

    Update Navigation.jsx

--- react-01/src/components/Navigation.jsx
+++ react-01/src/components/Navigation.jsx
@@ -9,13 +9,14 @@ function Navigation() {
 
   return (
     <nav className="nav">
-      <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle Navigation">
+      <button className="menu-toggle" onClick={toggleMenu} aria-label="Toggle navigation">
         &#9776;
       </button>
       <ul className={`nav-links ${menuOpen ? 'open' : ''}`}>
         <li><NavLink to="/" end onClick={() => setMenuOpen(false)}>Home</NavLink></li>
         <li><NavLink to="/about" onClick={() => setMenuOpen(false)}>About</NavLink></li>
-        <li><NavLink to="/tools" onClick={() => setMenuOpen(false)}>Tools</NavLink></li>
+        <li><NavLink to="/blog" onClick={() => setMenuOpen(false)}>Blog</NavLink></li>
+        <li><NavLink to="/shop" onClick={() => setMenuOpen(false)}>Shop</NavLink></li>
         <li><NavLink to="/contact" onClick={() => setMenuOpen(false)}>Contact</NavLink></li>
       </ul>
     </nav>
@@ -23,4 +24,3 @@ function Navigation() {
 }
 
 export default Navigation;
-
